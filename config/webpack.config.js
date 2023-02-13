e91ca796c753878065f80ae9db9aec4b1cf09939
Jonathan Müller <jonathan@fotokite.com>
2023-02-13 15:52:52 +0100

    config according to https://janus.conf.meetecho.com/docs/js-modules.html

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -339,6 +339,16 @@ module.exports = function (webpackEnv) {
     module: {
       strictExportPresence: true,
       rules: [
+        // janus.js does not use 'export' to provide its functionality to others, instead
+        // it creates a global variable called 'Janus' and expects consumers to use it.
+        // Let's use 'exports-loader' to simulate it uses 'export'.
+        {
+          test: require.resolve('janus-gateway'),
+          loader: 'exports-loader',
+          options: {
+            exports: 'Janus',
+          },
+        },
         // Handle node_modules packages that contain sourcemaps
         shouldUseSourceMap && {
           enforce: 'pre',
@@ -563,6 +573,10 @@ module.exports = function (webpackEnv) {
       ].filter(Boolean),
     },
     plugins: [
+      // janus.js does not use 'import' to access to the functionality of webrtc-adapter,
+      // instead it expects a global object called 'adapter' for that.
+      // Let's make that object available.
+      new webpack.ProvidePlugin({ adapter: ['webrtc-adapter', 'default'] }),
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
